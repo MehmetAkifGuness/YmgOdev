6f2c196293593bcbf14cbaa5f7dda80eb17ee71e
MehmetAkifGuness <gunesmehmetakif288@gmail.com>
2026-04-14 17:05:26 +0300

    feat: user and task repositories added

---
+++ src/main/java/com/gunes/docker/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.gunes.docker.repository;
+
+import com.gunes.docker.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
