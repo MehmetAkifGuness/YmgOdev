94543ddf2f5b761f3c3de32e500d804b659cbfc3
MehmetAkifGuness <gunesmehmetakif288@gmail.com>
2026-04-14 17:19:25 +0300

    feat: DataSeeder ve TaskController eklendi

---
+++ src/main/java/com/gunes/docker/controller/TaskController.java
@@ -0,0 +1,34 @@
+package com.gunes.docker.controller;
+
+import com.gunes.docker.entity.Task;
+import com.gunes.docker.repository.TaskRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+@CrossOrigin(origins = "*") 
+public class TaskController {
+
+    private final TaskRepository taskRepository;
+
+    public TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+}
